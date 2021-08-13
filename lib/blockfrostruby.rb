f3fbc330866c3f9df5cacad8da0e437246f7bae8
Ivan <ivan.papushoi@mail.ru>
2021-08-13 11:11:09 +0300

    Initial gem setup

---
+++ lib/blockfrostruby.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require_relative "blockfrostruby/version"
+
+module Blockfrostruby
+  class Error < StandardError; end
+  
+  class Check
+    def self.hi
+      puts 'Hello world'
+    end
+  end
+end
---
+++ spec/blockfrostruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Blockfrostruby do
+  it "has a version number" do
+    expect(Blockfrostruby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
