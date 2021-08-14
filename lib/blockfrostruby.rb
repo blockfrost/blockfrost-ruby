92d8526f9d7c676f1fa98b85bb6b65ac37f6b596
Ivan <ivan.papushoi@mail.ru>
2021-08-14 12:20:43 +0300

    Add get_health method

--- lib/blockfrostruby.rb
+++ lib/blockfrostruby.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true 
+# frozen_string_literal: true
 
 # gem build blockfrostruby
 # gem install ./blockfrostruby-0.1.0.gem
@@ -9,31 +9,39 @@ require 'json'
 
 module Blockfrostruby
   # Net::HTTP.get(uri).encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
-  # Check puts on the https://guides.rubygems.org/make-your-own-gem/
   # https://dog.ceo/api/breeds/list/all
-  # JSON.parse(Net::HTTP.get(URI("https://dog.ceo/api/breeds/list/all")))
   # Format = status body
 
   # Should be separate module with only verbs action
-  # params = { :limit => 10, :page => 3 }
   # puts res.body if res.is_a?(Net::HTTPSuccess)
 
   class Error < StandardError; end
-  
+  # raise error if body status error
+
   class Request
-    def self.get_response(url, params={})
+    def self.get_response(url, _params = {}, _headers = nil)
+      # params = { :limit => 10, :page => 3, :order => 'desc' }
       response = Net::HTTP.get_response(URI(url))
-      body = response.header.content_type == "application/json" ? JSON.parse(response.body) : response.body
+      body = response.header.content_type == 'application/json' ? JSON.parse(response.body) : response.body
       { status: response.code, body: body }
     end
 
-    def self.get_all_pages(url) 
+    def self.get_all_pages(url)
       # https://docs.ruby-lang.org/en/2.0.0/Net/HTTP.html
       # uri = URI('http://example.com/some_path?query=string')
       # Net::HTTP.start(uri.host, uri.port) do |http|
       #   request = Net::HTTP::Get.new uri
       #   response = http.request request # Net::HTTPResponse object
       # end
+    end
+  end
+
+  class CardanoMainNet
+    CARDANO_MAINNET_URL = "https://cardano-mainnet.blockfrost.io/api/v0/" # To config
+    # Needs to share methods with CardanoTestNet class, the same requests. Use Fabriq with same classes
+
+    def self.get_health
+      Request.get_response(CARDANO_MAINNET_URL + "health")
     end
   end
 end
