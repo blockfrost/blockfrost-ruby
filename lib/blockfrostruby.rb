5917819d973f43e18acea7c2148ebe70f4b74213
Ivan <ivan.papushoi@mail.ru>
2021-08-16 15:04:36 +0300

    Refactor code

--- lib/blockfrostruby.rb
+++ lib/blockfrostruby.rb
@@ -11,42 +11,42 @@ module Request
   require 'net/http'
   require 'json'
 
-  def self.get_response(url, project_id, _params = {}, _headers = nil)
-    # params = { :limit => 10, :page => 3, :order => 'desc' }
-    # response = Net::HTTP.get_response(URI(url))
-    uri = URI(url)
-    req = Net::HTTP::Get.new(uri)
-    req['project_id'] = project_id
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(req) }
-    format_response(response)
-  end
+  class << self
+    def get_response(url, project_id, _params = {}, _headers = nil)
+      # params = { :limit => 10, :page => 3, :order => 'desc' }
+      # response = Net::HTTP.get_response(URI(url))
+      uri = URI(url)
+      req = Net::HTTP::Get.new(uri)
+      req['project_id'] = project_id
+      # TIMEOUT ERROR
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http| http.request(req) }
+      format_response(response)
+    end
 
-  def self.get_all_pages(url)
-    # https://docs.ruby-lang.org/en/2.0.0/Net/HTTP.html
-    # uri = URI('http://example.com/some_path?query=string')
-    # Net::HTTP.start(uri.host, uri.port) do |http|
-    #   request = Net::HTTP::Get.new uri
-    #   response = http.request request # Net::HTTPResponse object
-    # end
-  end
+    def get_all_pages(url)
+      # https://docs.ruby-lang.org/en/2.0.0/Net/HTTP.html
+      # uri = URI('http://example.com/some_path?query=string')
+      # Net::HTTP.start(uri.host, uri.port) do |http|
+      #   request = Net::HTTP::Get.new uri
+      #   response = http.request request # Net::HTTPResponse object
+      # end
+    end
 
-  private
+    private
 
-  def self.format_response(response)
-    # resque from uncoded:
-    # Net::HTTP.get(uri).encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
-    body = response.header.content_type == 'application/json' ? JSON.parse(response.body) : response.body
-    { status: response.code, body: body } # In config return whole object, default this one
-    # Look in the JS implementation
+    def format_response(response)
+      # resque from uncoded:
+      # Net::HTTP.get(uri).encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
+      body = response.header.content_type == 'application/json' ? JSON.parse(response.body) : response.body
+      { status: response.code, body: body } # In config return whole object, default this one
+      # Look in the JS implementation
+    end
   end
 end
 
 
 module HealthEndpoints
   extend Request
-
-  @@url = ""
-  @@project_id = ""
 
   def self.set_url(value)
     @@url = value
@@ -56,16 +56,16 @@ module HealthEndpoints
     @@project_id = value
   end
 
-  def self.get_root
-    Request.get_response("#{self.url}/", @@project_id )
+  def get_root
+    Request.get_response("#{@@url}/", @project_id )
   end
 
   def get_health
-    Request.get_response("#{@@url}/health", @@project_id )
+    Request.get_response("#{@@url}/health", @project_id )
   end
 
-  def self.get_health_clock
-    Request.get_response("#{self.url}/health/clock", @@project_id )
+  def get_health_clock
+    Request.get_response("#{@@url}/health/clock", @project_id )
   end
 end
 
@@ -85,16 +85,18 @@ module Blockfrostruby
       HealthEndpoints.set_project_id(@project_id)
     end
 
-    def self.get_custom_url
-      # used when user wants to add something in the url manually
-      # extend Request
-    end
+    class << self
+      def get_custom_url
+        # used when user wants to add something in the url manually
+        # extend Request
+      end
 
-    private
+      private
 
-    def self.set_vars_for_endpoints_modules(modules)
-      # TODO: refactor and implement setter for all endpoints modules
-      # Maybe enpoints modules can be grouped in array
+      def set_vars_for_endpoints_modules(modules)
+        # TODO: refactor and implement setter for all endpoints modules
+        # Maybe enpoints modules can be grouped in array
+      end
     end
   end
 
