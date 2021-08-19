0193e445f4d9c41abf797a67e11454032e09b7ef
Ivan <ivan.papushoi@mail.ru>
2021-08-19 12:07:06 +0300

    Refactor: remove useless

--- lib/blockfrostruby/request.rb
+++ lib/blockfrostruby/request.rb
@@ -7,12 +7,12 @@ require_relative 'blockfrostruby/configuration'
 module Request
   # REQUIRE CONFIG WITH METHOD WHICH CAN USE CONFIG
   # SHOULD HAVE MANAGER METHOD
-  include Configuration 
+  include Configuration
 
   class << self
     def get_response(url, project_id, params = {}, _headers = nil)
       # response = Net::HTTP.get_response(URI(url))
-      
+
       # url = add_params_to_url(url, params)
       # uri = URI(url)
       # req = Net::HTTP::Get.new(uri)
@@ -58,35 +58,8 @@ module Request
     end
 
     def define_params(params, config)
-      default_config = {
-        return_whole_object_in_request: false,
-        parallel_requests: 5,
-        use_desc_order_as_default: false,
-        default_count_per_page: 100
-      }
-      # params = { order: 'desc', count: 2 }
       permitted_params = permitted(params)
       result = permitted_params
-      puts "result: #{result}"
-      result[:order] = result[:order]&.downcase
-
-      order_param_in_default_config = default_config[:use_desc_order_as_default] == false ? 'asc' : 'desc'
-      order_param_in_object_config = config[:use_desc_order_as_default] == false ? 'asc' : 'desc'
-      
-      puts "default config: #{order_param_in_default_config}"
-      puts "object config: #{order_param_in_object_config}"
-      result[:order] = order_param_in_object_config if order_param_in_object_config != order_param_in_default_config && order_param_in_object_config 
-      result[:order] = permitted_params[:order] if permitted_params[:order] != result[:order]
-      puts "result[:order] = #{result[:order]}"
-      #result.delete(:order) if result[:order] == nil
-      result.delete(:order) if result[:order]&.downcase == order_param_in_default_config || result[:order] == nil
-      result
-    end
-
-    def define_params_2(params, config)
-      permitted_params = params.transform_keys(&:to_sym).slice(:order, :page, :count)
-      result = permitted_params
-      puts result[:order]
       result[:order] = define_order(result[:order], config)
       result.delete(:order) if result[:order] == nil
       result
@@ -99,23 +72,15 @@ module Request
         use_desc_order_as_default: false,
         default_count_per_page: 100
       }
-      #return order_param.downcase if order_param.downcase in ['asc', 'desc']
-      #order_param = order_param&.downcase
       order_in_default_config = default_config[:use_desc_order_as_default] == false ? 'asc' : 'desc'
       order_in_object_config = object_config[:use_desc_order_as_default] == false ? 'asc' : 'desc'
-      # object = asc, order_param = desc
-
-      #puts order_param
-      puts "order_param: #{order_param}, object: #{order_in_object_config}, default: #{order_in_default_config} "
-      result = order_in_object_config if order_param == nil 
-
+      result = order_in_object_config if order_param == nil
       result = order_param if order_param != nil
-      result = nil if result == order_in_default_config  
+      result = nil if result == order_in_default_config
       result
-
     end
 
-    # Checks: 
+    # Checks:
       # 1. Pass ({}, {use_desc_order_as_default: false}), should return {}
       # 2. Pass ({}, {use_desc_order_as_default: true}), should return {order: desc}
       # 3. Pass ({ order: 'desc' }, {use_desc_order_as_default: true}), should return {order: desc}
@@ -128,8 +93,5 @@ module Request
 
     def changed_params
     end
-    
-    
-
   end
 end
