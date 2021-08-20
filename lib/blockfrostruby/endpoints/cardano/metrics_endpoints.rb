a10c5b3145a9b1ca5bf7e089f552b2db5684ad4a
Ivan <ivan.papushoi@mail.ru>
2021-08-20 12:21:57 +0300

    Refactor: remove useless params from metrics endpoints

--- lib/blockfrostruby/endpoints/cardano/metrics_endpoints.rb
+++ lib/blockfrostruby/endpoints/cardano/metrics_endpoints.rb
@@ -5,11 +5,11 @@ require_relative '../../../blockfrostruby/request'
 module MetricsEndpoints
   extend Request
 
-  def get_metrics(params = {})
-    Request.get_response("#{@url}/metrics", @project_id, params)
+  def get_metrics
+    Request.get_response("#{@url}/metrics", @project_id)
   end
 
-  def get_metrics_endpoints(params = {})
-    Request.get_response("#{@url}/metrics/endpoints", @project_id, params)
+  def get_metrics_endpoints
+    Request.get_response("#{@url}/metrics/endpoints", @project_id)
   end
 end
