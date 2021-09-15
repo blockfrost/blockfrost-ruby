3c3a483e73ec8a523f934a8ad22fb40e1b011a6b
Ivan <ivan.papushoi@mail.ru>
2021-09-15 15:48:47 +0300

    Add addresses_endpoints doc

--- lib/blockfrostruby/endpoints/cardano/addresses_endpoints.rb
+++ lib/blockfrostruby/endpoints/cardano/addresses_endpoints.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Example: addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz
+
 require_relative '../../../blockfrostruby/request'
 require_relative '../../../blockfrostruby/params'
 
@@ -7,21 +9,37 @@ module AddressesEndpoints
   extend Request
   extend Params
 
-  # Example: addr1qxqs59lphg8g6qndelq8xwqn60ag3aeyfcp33c2kdp46a09re5df3pzwwmyq946axfcejy5n4x0y99wqpgtp2gd0k09qsgy6pz
-
+  # Calls get request on (@url)/addresses/(address).
+  #
+  # @param address [String] will be added to the url for get request.
+  # @return [Hash] formatted result with status and body keys.
   def get_address(address)
     Request.get_response("#{@url}/addresses/#{address}", @project_id)
   end
 
+  # Calls get request on (@url)/addresses/(address)/total.
+  #
+  # @param address [String] will be added to the url for get request.
+  # @return [Hash] formatted result with status and body keys.
   def get_address_details(address)
     Request.get_response("#{@url}/addresses/#{address}/total", @project_id)
   end
 
+  # Calls get request on (@url)/addresses/(address)/utxos.
+  #
+  # @param address [String] will be added to the url for get request.
+  # @param params [Hash] - params passed by user.
+  # @return [Hash] formatted result with status and body keys.
   def get_address_utxos(address, params = {})
     params = Params.define_params(params, @config)
     Request.get_response("#{@url}/addresses/#{address}/utxos", @project_id, params)
   end
 
+  # Calls get request on (@url)/addresses/(address)/transactions.
+  #
+  # @param address [String] will be added to the url for get request.
+  # @param params [Hash] - params passed by user.
+  # @return [Hash] formatted result with status and body keys.
   def get_address_transactions(address, params = {})
     params = Params.define_params(params, @config)
     Request.get_response("#{@url}/addresses/#{address}/transactions", @project_id, params)
