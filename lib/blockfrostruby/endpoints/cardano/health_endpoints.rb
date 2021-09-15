f9088c750a11e98322ccb4e62e2d046e1ec663ea
Ivan <ivan.papushoi@mail.ru>
2021-09-15 16:47:04 +0300

    Add health enpoints docs

--- lib/blockfrostruby/endpoints/cardano/health_endpoints.rb
+++ lib/blockfrostruby/endpoints/cardano/health_endpoints.rb
@@ -5,14 +5,23 @@ require_relative '../../../blockfrostruby/request'
 module HealthEndpoints
   extend Request
 
+  # Calls get request on (@url)/.
+  #
+  # @return [Hash] formatted result with status and body keys.
   def get_root
     Request.get_response("#{@url}/", @project_id)
   end
 
+  # Calls get request on (@url)/health.
+  #
+  # @return [Hash] formatted result with status and body keys.
   def get_health
     Request.get_response("#{@url}/health", @project_id)
   end
 
+  # Calls get request on (@url)/health/clock.
+  #
+  # @return [Hash] formatted result with status and body keys.
   def get_health_clock
     Request.get_response("#{@url}/health/clock", @project_id)
   end
