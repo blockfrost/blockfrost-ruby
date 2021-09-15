b5eee0b7a137b67d807f56ad300f593031b46131
Ivan <ivan.papushoi@mail.ru>
2021-09-15 16:51:20 +0300

    Add metadata_endpoints docs

--- lib/blockfrostruby/endpoints/cardano/ledger_endpoints.rb
+++ lib/blockfrostruby/endpoints/cardano/ledger_endpoints.rb
@@ -5,6 +5,9 @@ require_relative '../../../blockfrostruby/request'
 module LedgerEndpoints
   extend Request
 
+  # Calls get request on (@url)/genesis.
+  #
+  # @return [Hash] formatted result with status and body keys.
   def get_genesis
     Request.get_response("#{@url}/genesis", @project_id)
   end
