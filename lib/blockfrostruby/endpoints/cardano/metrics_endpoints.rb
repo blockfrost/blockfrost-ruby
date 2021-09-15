a5bf91b77d4e67efa79d57b22b96135d430e7478
Ivan <ivan.papushoi@mail.ru>
2021-09-15 17:09:43 +0300

    Add nutlink_endpoints docs

--- lib/blockfrostruby/endpoints/cardano/metrics_endpoints.rb
+++ lib/blockfrostruby/endpoints/cardano/metrics_endpoints.rb
@@ -5,10 +5,16 @@ require_relative '../../../blockfrostruby/request'
 module MetricsEndpoints
   extend Request
 
+  # Calls get request on (@url)/metrics.
+  #
+  # @return [Hash] formatted result with status and body keys.
   def get_metrics
     Request.get_response("#{@url}/metrics", @project_id)
   end
 
+  # Calls get request on (@url)/metrics.
+  #
+  # @return [Hash] formatted result with status and body keys.
   def get_metrics_endpoints
     Request.get_response("#{@url}/metrics/endpoints", @project_id)
   end
