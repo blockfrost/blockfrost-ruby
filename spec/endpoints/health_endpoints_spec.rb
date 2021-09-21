5c03b9ca7ebb21717248d0eb97458665192dc64a
Ivan <ivan.papushoi@mail.ru>
2021-09-21 19:26:23 +0300

    Add cardano mainnet healthendpoints specs

--- spec/endpoints/health_endpoints_spec.rb
+++ spec/endpoints/health_endpoints_spec.rb
@@ -6,6 +6,7 @@ require_relative '../../lib/blockfrostruby/endpoints/cardano/health_endpoints'
 
 RSpec.describe HealthEndpoints do
   let(:blockfrost_mainnet) { Blockfrostruby::CardanoMainNet.new(ENV['BF_MAINNET_PROJECT_ID']) }
+  let(:blockfrost_mainnet_invalid) { Blockfrostruby::CardanoMainNet.new('invalid project_id') }
 
   context 'CardanoMainNet object' do
     context 'with valid project_id' do
@@ -14,6 +15,32 @@ RSpec.describe HealthEndpoints do
           response = blockfrost_mainnet.get_health
           expect(response[:status]).to eq(200)
           expect(response[:body]).to eq({ is_healthy: true })
+        end
+      end
+
+      context 'when calls get_root method' do
+        it 'return status 200 and response includes an url' do
+          response = blockfrost_mainnet.get_root
+          expect(response[:status]).to eq(200)
+          expect(response[:body]).to include(:url =>"https://blockfrost.io/")
+        end
+      end
+
+      context 'when calls get_health_clock method' do
+        it 'return status 200 and response includes a server time' do
+          response = blockfrost_mainnet.get_health_clock
+          expect(response[:status]).to eq(200)
+          expect(response[:body]).to include(:server_time)
+        end
+      end
+    end
+
+    context 'with invalid project_id' do 
+      context 'when calls get_health method' do
+        it 'returns 403 status and body includes error' do 
+          response = blockfrost_mainnet_invalid.get_health
+          expect(response[:status]).to eq(403)
+          expect(response[:body]).to include(:error)
         end
       end
     end
